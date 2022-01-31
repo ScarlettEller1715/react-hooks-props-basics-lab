03b028fe5a3f545ffb6fc1c476740edc7c20d061
Scarlett Eller <rq6715@gmail.com>
2022-01-31 14:10:09 -0500

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,27 @@
 import React from "react";
 
-function About() {
+function Links({ github, linkedin }) {
+  return (
+    <h3>
+      <a href={github}>{github}</a>
+      <a href={linkedin}>{linkedin}</a>
+    </h3>
+  )
+}
+
+function BioCreation({ bio }) {
+  return (
+    bio ? <p>{bio}</p> : null
+  )
+}
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <BioCreation bio={props.bio} />
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
